2f7316ced4cb4a0b8ff60867e2ac9b3288b8889b
Anis Nouri <nouurianis@gmail.com>
2023-03-01 21:11:03 +0000

    add rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -46,6 +46,11 @@ LOGGER.addHandler(cw_handler)
 LOGGER.info("Test log")
 """
 
+# Rollbar ----------------
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # HoneyComb ----------------
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -85,11 +90,36 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+"""
 @app.after_request
 def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+"""
+# Rollbar ----------------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
